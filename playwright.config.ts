c16513a5ff468ac1470af7b49de2ad7497ba6438
adewar <andrewgdewar@gmail.com>
2025-03-10 17:17:28 -0600

    Test: Add support for multiple users

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,80 +1,97 @@
-import { defineConfig, devices } from '@playwright/test';
-import 'dotenv/config';
+import { defineConfig, devices } from "@playwright/test";
+import "dotenv/config";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    testDir: './tests',
-    fullyParallel: false,
-    forbidOnly: false,
-    retries: process.env.CI ? 1 : 0,
-    workers: 1,
-    reporter: process.env.CI
-        ? [
-            [
-                'playwright-ctrf-json-reporter',
-                { useDetails: true, outputDir: 'playwright-ctrf', outputFile: 'playwright-ctrf.json' },
-            ],
-            ['html', { outputFolder: 'playwright-report' }],
-            ['@currents/playwright'],
-        ]
-        : 'list',
-    timeout: process.env.CI ? 60000 : 30000,
-    expect: { timeout: process.env.CI ? 60000 : 20000 },
-    use: {
-        testIdAttribute: 'data-ouia-component-id',
-        launchOptions: {
-            args: ['--use-fake-device-for-media-stream'],
-        },
-        ...(process.env.TOKEN
-            ? {
-                extraHTTPHeaders: {
-                    Authorization: process.env.TOKEN,
-                },
-            }
-            : {}),
-        baseURL: process.env.BASE_URL,
-        trace: 'on',
-        screenshot: 'on',
-        video: 'on',
-        ignoreHTTPSErrors: true,
-        ...process.env.PROXY ? {
-            proxy: {
-                server: process.env.PROXY,
-            }
-        } : {}
+  testDir: "./tests",
+  fullyParallel: false,
+  forbidOnly: false,
+  retries: process.env.CI ? 1 : 0,
+  workers: 1,
+  reporter: process.env.CI
+    ? [
+        [
+          "playwright-ctrf-json-reporter",
+          {
+            useDetails: true,
+            outputDir: "playwright-ctrf",
+            outputFile: "playwright-ctrf.json",
+          },
+        ],
+        ["html", { outputFolder: "playwright-report" }],
+        ["@currents/playwright"],
+      ]
+    : "list",
+  timeout: process.env.CI ? 60000 : 30000,
+  expect: { timeout: process.env.CI ? 60000 : 20000 },
+  use: {
+    testIdAttribute: "data-ouia-component-id",
+    launchOptions: {
+      args: ["--use-fake-device-for-media-stream"],
     },
-    projects: [
-        { name: 'setup', testMatch: /.*\.setup\.ts/ },
-        {
-            name: 'chromium',
-            grepInvert: !!process.env.PROD ? [/preview-only/, /switch-to-preview/] : [/switch-to-preview/],
-            use: {
-                ...devices['Desktop Chrome'],
-                storageState: `.auth/${process.env.USER1USERNAME}.json`,
-            },
-            dependencies: ['setup'],
-        },
-        ...!!process.env.PROD ?
-            [{
-                name: 'Switch to preview',
-                grep: [/switch-to-preview/],
-                use: {
-                    ...devices['Desktop Chrome'],
-                    storageState: `.auth/${process.env.USER1USERNAME}.json`,
-
-                },
-                dependencies: ['setup'],//'chromium',
-            },
-            {
-                name: 'Run preview only',
-                grep: [/preview-only/],
-                use: {
-                    ...devices['Desktop Chrome'],
-                    storageState: `.auth/${process.env.USER1USERNAME}.json`,
-                },
-                dependencies: ['Switch to preview'],
-            }] : [],
-    ],
+    ...(process.env.TOKEN
+      ? {
+          extraHTTPHeaders: {
+            Authorization: process.env.TOKEN,
+          },
+        }
+      : {}),
+    baseURL: process.env.BASE_URL,
+    trace: "on",
+    screenshot: "on",
+    video: "on",
+    ignoreHTTPSErrors: true,
+    ...(process.env.PROXY
+      ? {
+          proxy: {
+            server: process.env.PROXY,
+          },
+        }
+      : {}),
+  },
+  projects: [
+    { name: "setup", testMatch: /.*\.setup\.ts/ },
+    {
+      name: "Admin tests",
+      grepInvert: [/read-only/], //!!process.env.PROD ? [/preview-only/, /switch-to-preview/] : [/switch-to-preview/],
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: `.auth/${process.env.USER1USERNAME}.json`,
+      },
+      dependencies: ["setup"],
+    },
+    {
+      name: "Read-only user tests",
+      grep: [/read-only/],
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: `.auth/${process.env.USER2USERNAME}.json`,
+      },
+      dependencies: ["setup"],
+    },
+    // ...(!!process.env.PROD
+    //   ? [
+    //       {
+    //         name: "Switch to preview",
+    //         grep: [/switch-to-preview/],
+    //         use: {
+    //           ...devices["Desktop Chrome"],
+    //           storageState: `.auth/${process.env.USER1USERNAME}.json`,
+    //         },
+    //         dependencies: ["setup"], //'chromium',
+    //       },
+    //       {
+    //         name: "Run preview only",
+    //         grep: [/preview-only/],
+    //         use: {
+    //           ...devices["Desktop Chrome"],
+    //           storageState: `.auth/${process.env.USER1USERNAME}.json`,
+    //         },
+    //         dependencies: ["Switch to preview"],
+    //       },
+    //     ]
+    //   : []),
+  ],
 });
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,9 +2,10 @@ import { expect, test as setup, type Page } from "@playwright/test";
 import {
   throwIfMissingEnvVariables,
   closePopupsIfExist,
-  switchToUser,
+  setCookieForUser,
   logInWithUsernameAndPassword,
   ensureNotInPreview,
+  logout,
 } from "./helpers/loginHelpers";
 import { describe } from "node:test";
 
@@ -13,22 +14,25 @@ describe("Setup", async () => {
     expect(() => throwIfMissingEnvVariables()).not.toThrow();
   });
 
-  setup("Authenticate user 1", async ({ page }) => {
+  setup("Authenticate all users", async ({ page }) => {
     await closePopupsIfExist(page);
     await logInWithUsernameAndPassword(
       page,
       process.env.USER1USERNAME,
       process.env.USER1PASSWORD
     );
-    // Example of how to add another user
-    // await logout(page)
-    // await logInWithUsernameAndPassword(
-    //     page,
-    //     process.env.USER2USERNAME,
-    //     process.env.USER2PASSWORD
-    // );
-    // Example of how to switch to said user
-    await switchToUser(page, process.env.USER1USERNAME!);
+    await setCookieForUser(page, process.env.USER1USERNAME!);
+    await ensureNotInPreview(page);
+
+    await logout(page);
+
+    await logInWithUsernameAndPassword(
+      page,
+      process.env.USER2USERNAME,
+      process.env.USER2PASSWORD
+    );
+
+    await setCookieForUser(page, process.env.USER2USERNAME!);
     await ensureNotInPreview(page);
     // Other users for other tests can be added below after logging out
   });
